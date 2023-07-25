1dd4d3aebaa9aba6f8bf8bba937c91efdc1461b5
arthurchina92 <arthurchinaa@gmail.com>
2023-07-25 13:54:17 -0600

    Index and step2

--- main.js
+++ main.js
@@ -3,9 +3,3 @@ const nextStepButton = document.getElementById("personal-info-button");
 nextStepButton.addEventListener("click", function () {
   window.location.href = "step2.html";
 });
-
-const backStepButton2 = document.getElementById("back-button-step2");
-
-backStepButton2.addEventListener("click", function () {
-  window.location.href = "index.html";
-});
---
+++ step2.js
@@ -0,0 +1,10 @@
+const backStepButton2 = document.getElementById("back-button-step2");
+const nextStepButton2 = document.getElementById("selectPlan-button");
+
+backStepButton2.addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+nextStepButton2.addEventListener("click", function () {
+  window.location.href = "step3.html";
+});
